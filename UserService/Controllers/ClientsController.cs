ad06037eb3df884ca73fd598517a245c9168e57f
Alfonso Jimenez <alfjim99@hotmail.com>
2024-11-02 23:18:12 -0500

    feat: added client controller

---
+++ UserService/Controllers/ClientsController.cs
@@ -0,0 +1,24 @@
+﻿namespace UserService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ClientsController : ControllerBase
+{
+    private readonly ILogger<ClientsController> _logger;
+    private readonly IClientService _clientService;
+
+    public ClientsController(ILogger<ClientsController> logger, IClientService clientService)
+    {
+        _logger = logger;
+        _clientService = clientService;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(200, Type = typeof(ClientViewModel))]
+    [ProducesResponseType(404)]
+    public async Task<ActionResult<ClientViewModel>> CreateClientAsync([FromBody] ClientRequest clientRequest)
+    {
+        var client = await _clientService.CreateClientAsync(clientRequest);
+        return Ok(client);
+    }
+}
--- UserService/GlobalUsings.cs
+++ UserService/GlobalUsings.cs
@@ -1,9 +1,10 @@
-﻿global using Microsoft.EntityFrameworkCore;
+﻿global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
 global using System.ComponentModel.DataAnnotations;
 global using UserService.Models;
+global using UserService.Repositories;
 global using UserService.Repositories.Interfaces;
+global using UserService.Services;
 global using UserService.Services.Interfaces;
 global using UserService.ViewModels;
 global using UserService.ViewModels.Request;
-global using UserService.Repositories;
-global using UserService.Services;
